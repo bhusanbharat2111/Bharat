03fdc014b052dc2e0cf9b618b91a4ea6003160e7
Bharat <bhusanbharat2111@gmail.com>
2022-08-02 10:55:54 +0530

    exception String

--- src/training/day1/BasicProgram.java
+++
@@ -1,9 +0,0 @@
-package training.day1;
-
-// this program only prints a statement
-public class BasicProgram {
-	public static void main(String[] args) {
-		System.out.println("This is a very basic program which only prints a line of statement");
-	}
-
-}
