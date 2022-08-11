e50ac81347b6b7523a52eb537f9807096816b2eb
B Bharat Bhusan Subudhi <109270093+bhusanbharat2111@users.noreply.github.com>
2022-08-11 15:51:15 +0530

    Update DemoFile.java

--- src/training/filehandling/DemoFile.java
+++ src/training/filehandling/DemoFile.java
@@ -8,26 +8,30 @@ import java.io.FileWriter;
 
 public class DemoFile {
 	public static void main(String[] args) throws IOException {
-		BufferedWriter buffer = new BufferedWriter(
-				new FileWriter("D:\\NewProject1\\src\\training\\filehandling\\TextFile2.txt"));
-		/**
-		 * Using Write method to insert data into the file.
-		 */
-		buffer.write("hello");
-		buffer.write("\nFile has been created successfully");
+		BufferedWriter buffer = null;
+		BufferedReader bufferr = null;
+		try {
+			buffer = new BufferedWriter(new FileWriter("TextFile2.txt"));
+			/**
+			 * Using Write method to insert data into the file.
+			 */
+			buffer.write("hello");
+			buffer.write("\nFile has been created successfully");
 
-		buffer.close();
-
-		BufferedReader bufferr = new BufferedReader(
-				new FileReader("D:\\NewProject1\\src\\training\\filehandling\\TextFile2.txt"));
-		/**
-		 * Using readLine() method to get the data present in the file.
-		 */
-		System.out.println(bufferr.readLine());
-		String Details = null;
-		while ((Details = bufferr.readLine()) != null) {
-			System.out.println(Details);
+			bufferr = new BufferedReader(new FileReader("TextFile2.txt"));
+			/**
+			 * Using readLine() method to get the data present in the file.
+			 */
+			System.out.println(bufferr.readLine());
+			String Details = null;
+			while ((Details = bufferr.readLine()) != null) {
+				System.out.println(Details);
+			}
+		} catch (Exception e) {
+			System.out.println("Invalid Input!!");
+		} finally {
+			buffer.close();
+			bufferr.close();
 		}
-		bufferr.close();
 	}
 }
