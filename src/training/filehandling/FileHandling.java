38c4f7c8e911edc8c35aa05b1a22961c6c401878
B Bharat Bhusan Subudhi <109270093+bhusanbharat2111@users.noreply.github.com>
2022-08-11 15:52:43 +0530

    Update FileHandling.java

--- src/training/filehandling/FileHandling.java
+++ src/training/filehandling/FileHandling.java
@@ -7,34 +7,38 @@ import java.util.Scanner;
 
 public class FileHandling {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// created a object of File class and given the path
-		File f = new File("D:\\NewProject1\\src\\training\\filehandling\\Bharat.txt");
-		// checking if the file is present or not
-		if (f.createNewFile()) {
-			System.out.println("Bharat.txt <- Successfully created");
-		} else {
-			System.out.println("Bharat.txt<- File already exists");
-		}
+		File f = new File("Bharat.txt");
+		FileWriter fw = null;
+		Scanner sc = null;
 
-		// writing in the file
-		FileWriter fw = new FileWriter("D:\\NewProject1\\src\\training\\filehandling\\Bharat.txt");
-		fw.write("Anything - used to refer to a thing, no matter what.\"nobody was saying anything\""
-				+ "used for emphasis." + "\"I was ready for anything\"used to indicate a range."
-				+ "he trains anything from seven to eight hours a day");
-		fw.close();
-		// printing the data available in that file
-		Scanner sc = new Scanner(f);
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			System.out.println(line);
+		try {
+			// checking if the file is present or not
+			if (f.createNewFile()) {
+				System.out.println("Bharat.txt <- Successfully created");
+			} else {
+				System.out.println("Bharat.txt<- File already exists");
+			}
+
+			// writing in the file
+			fw = new FileWriter("Bharat.txt");
+			fw.write("Anything - used to refer to a thing, no matter what.\"nobody was saying anything\""
+					+ "used for emphasis." + "\"I was ready for anything\"used to indicate a range."
+					+ "he trains anything from seven to eight hours a day");
+			// printing the data available in that file
+			sc = new Scanner(f);
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Invalid Input!!");
+		} finally {
+			sc.close();
 		}
-		sc.close();
 //		if (f.delete()) {
 //			System.out.println("I have deleted: " + f.getName());
 //		} else {
 //			System.out.println("Some problem occurred while deleting the file");
 //		}
-	}
-
-}
