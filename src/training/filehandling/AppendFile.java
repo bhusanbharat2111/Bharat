8ee589943597d6193e68c22679b9cdac6345ff63
B Bharat Bhusan Subudhi <109270093+bhusanbharat2111@users.noreply.github.com>
2022-08-11 15:50:09 +0530

    Update AppendFile.java

--- src/training/filehandling/AppendFile.java
+++ src/training/filehandling/AppendFile.java
@@ -8,23 +8,22 @@ import java.io.IOException;
 
 public class AppendFile {
 	public static void main(String[] args) throws IOException {
-		try (FileWriter fw = new FileWriter("D:\\NewProject1\\src\\training\\filehandling\\TextFile2.txt", true);
-				BufferedWriter bw = new BufferedWriter(fw)) {
-			bw.write("\nNew Text Appended ");
+		BufferedReader bufferr1 = null;
+		try (FileWriter fw = new FileWriter("TextFile2.txt", true); BufferedWriter bw = new BufferedWriter(fw)) {
+			bw.write("\nNew Text Appended. ");
 
 			bw.write("Appending successfull!!!");
+			bufferr1 = new BufferedReader(new FileReader("TextFile2.txt"));
 
+			System.out.println(bufferr1.readLine());
+			String Details = null;
+			while ((Details = bufferr1.readLine()) != null) {
+				System.out.println(Details);
+			}
 		} catch (IOException e) {
 			System.out.println("Invalid Input!!");
+		} finally {
+			bufferr1.close();
 		}
-		BufferedReader bufferr1 = new BufferedReader(
-				new FileReader("D:\\NewProject1\\src\\training\\filehandling\\TextFile2.txt"));
-
-		System.out.println(bufferr1.readLine());
-		String Details = null;
-		while ((Details = bufferr1.readLine()) != null) {
-			System.out.println(Details);
-		}
-		bufferr1.close();
 	}
 }
